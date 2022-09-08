d962d52b40bc89ca51ab78b0bcfc40ef62d1b9aa
jontygrbac <s3659145@student.rmit.edu.au>
2022-09-08 20:02:54 +1000

    Local -> Remote

---
+++ src/test/java/com/sept/sept_challenge/SeptChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sept.sept_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
